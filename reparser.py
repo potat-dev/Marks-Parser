dd566e6881d118c535a5bedd7827655ed14060d5
Den <denis100703@gmail.com>
2022-03-18 05:19:26 +0300

    some code formatting

--- reparser.py
+++ reparser.py
@@ -1,19 +1,6 @@
 from datetime import datetime as dt
 import json as j
 
-with open("data.json", "r", encoding="Windows-1251") as f:
-    data = j.loads(f.read())
-marks = []
-for period in data:
-    for lesson in period["lessons"]:
-        marks += lesson["marks"]
-marks = [{
-    "m": m["mark"],
-    "d": m["time"]
-} for m in sorted(
-    marks, key=lambda mark: int(mark["time"])
-)]
-
 def period_aver(d1, d2):
     summ, count = 0, 0
     for i in marks:
@@ -23,13 +10,23 @@ def period_aver(d1, d2):
                 count += 1
     return round(summ / count, 3)
 
-print(period_aver(dt(2017,9,1),dt(2018,6,1)))
-print(period_aver(dt(2018,9,1),dt(2019,6,1)))
-print(period_aver(dt(2019,9,1),dt(2020,6,1)))
-print(period_aver(dt(2020,9,1),dt(2021,6,1)))
 
+with open("output/data.json", "r", encoding="Windows-1251") as f:
+    data = j.loads(f.read())
 
+marks = []
+for period in data:
+    for lesson in period["lessons"]:
+        marks += lesson["marks"]
 
+marks = [{
+    "m": m["mark"],
+    "d": m["time"]
+} for m in sorted(
+    marks, key=lambda mark: int(mark["time"])
+)]
 
-
-# print(marks)
+print(period_aver(dt(2017, 9, 1), dt(2018, 6, 1)))
+print(period_aver(dt(2018, 9, 1), dt(2019, 6, 1)))
+print(period_aver(dt(2019, 9, 1), dt(2020, 6, 1)))
+print(period_aver(dt(2020, 9, 1), dt(2021, 6, 1)))
